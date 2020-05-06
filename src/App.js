94fe60c249cd0f32904da46f91886cd4e5b81000
Santiago Enciso <whereispolaris@gmail.com>
2020-05-06 08:24:28 -0500

    Implemented lifecycle method to make API request, added paragrapphs to display the information about each movie.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			movies: []
+		};
+	}
+	componentDidMount() {
+		let dataURL = 'http://headlesswp.local/wp-json/wp/v2/movies?_embed';
+		fetch(dataURL)
+			.then(res => res.json())
+			.then(res => {
+				this.setState({
+					movies: res
+				});
+			});
+	}
+	render() {
+		let movies = this.state.movies.map((movie, index) => {
+			return (
+				<div key={index}>
+					<img
+						src={
+							movie._embedded['wp:featuredmedia'][0].media_details.sizes.large
+								.source_url
+						}
+					/>
+					<p>
+						<strong>Title:</strong> {movie.title.rendered}
+					</p>
+					<p>
+						<strong>Release Year:</strong> {movie.acf.release_year}
+					</p>
+					<p>
+						<strong>Rating:</strong> {movie.acf.rating}
+					</p>
+					<div>
+						<strong>Description:</strong>
+						<div dangerouslySetInnerHTML={{ __html: movie.acf.description }} />
+					</div>
+				</div>
+			);
+		});
+		return (
+			<div>
+				<h2>Star Wars Movies</h2>
+				{movies}
+			</div>
+		);
+	}
 }
-
 export default App;
